561a436290ca94e8e344afc1548a02ebdb3c7714
faithwest <faithchepkoech425@gmail.com>
2023-09-29 13:43:37 +0300

    complete

---
+++ index.js
@@ -0,0 +1,3 @@
+/*Explain the concept of lexical scoping: refers to the principle that determines where functions are declared*/
+
+/*when a function is declared,it retains reference to outer scope  this creates chain of scope hence the lexical scoop informing the scope chain*/
